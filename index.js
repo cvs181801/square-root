0e02c6c65ea3b623c109ca6dca68bfdcd0efb8e1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-01 17:17:09 -0700

    add trendering

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //grab elements from the DOM
+const number = document.getElementById("numberBox");
+const goButton = document.getElementById("go-btn");
+const result = document.getElementById("resultDiv");
+const renderResult = document.createElement("p");
 
 //get the square root of the number the user enters
 
 function getSquareRoot(num) {
-    return Math.sqrt(num);
+    renderResult.innerHTML = "";
+    renderResult.innerHTML = Math.sqrt(num);
+    result.appendChild(renderResult);
 }
 
-console.log(getSquareRoot(49));
+console.log(getSquareRoot(number.value));
