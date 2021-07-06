c361ce60f170a1f1bc4b1874801826f6f6828db8
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 11:06:18 -0700

    add the .length

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-//grab elements from the DOM
+// //grab elements from the DOM
 
-const number1 = document.getElementById("numberBox1");
-const number2 = document.getElementById("numberBox2");
-const number3 = document.getElementById("numberBox3");
-const number4 = document.getElementById("numberBox4");
-const number5 = document.getElementById("numberBox5");
+// const number1 = document.getElementById("numberBox1");
+// const number2 = document.getElementById("numberBox2");
+// const number3 = document.getElementById("numberBox3");
+// const number4 = document.getElementById("numberBox4");
+// const number5 = document.getElementById("numberBox5");
 
-const goButton = document.getElementById("go-btn");
-const clearButton = document.getElementById("clear-btn");
-const result = document.getElementById("resultDiv");
-let renderResult = document.createElement("p");
+// const goButton = document.getElementById("go-btn");
+// const clearButton = document.getElementById("clear-btn");
+// const result = document.getElementById("resultDiv");
+// let renderResult = document.createElement("p");
 
-//get the square root of each the number the user enters, then sum up the total
+// //get the square root of each the number the user enters, then sum up the total
 
-function getSquareRoot(numbers) {
-    clearButton.classList.toggle("hidden");
-    goButton.classList.toggle("hidden");
-    for (let i = 0; i<numbers.length; i++) {
-        let renderResult = document.createElement("p");
-        renderResult.innerHTML = "";
-        renderResult.innerHTML = `The Square Root of ${numbers[i]} is ${Math.sqrt(numbers[i])}`;
-        result.append(renderResult);
-        //console.log(Math.sqrt(numbers[i]));
-    }
-}
+// function getSquareRoot(numbers) {
+//     clearButton.classList.toggle("hidden");
+//     goButton.classList.toggle("hidden");
+//     for (let i = 0; i<numbers.length; i++) {
+//         let renderResult = document.createElement("p");
+//         renderResult.innerHTML = "";
+//         renderResult.innerHTML = `The Square Root of ${numbers[i]} is ${Math.sqrt(numbers[i])}`;
+//         result.append(renderResult);
+//         //console.log(Math.sqrt(numbers[i]));
+//     }
+// }
 
 function clearArray(numbers) {
     // console.log(numbers);
@@ -31,22 +31,24 @@ function clearArray(numbers) {
     // clearButton.classList.toggle("hidden");
     // goButton.classList.toggle("hidden");
     // return numbers;
-    for (let i=0; i<numbers; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         //renderResult.innerHTML = "";
         console.log("yo");
     }
 }
 
+console.log(clearArray([4, 4, 4, 4]));
+
 //console.log(getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]));
 
-goButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log("go button worked!!");
-    return getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
-})
+// goButton.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log("go button worked!!");
+//     return getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
+// })
 
-clearButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log("clear button worked!!");
-    return clearArray([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
-})
+// clearButton.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log("clear button worked!!");
+//     return clearArray([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
+// })
