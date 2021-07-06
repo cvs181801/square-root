381bcd60576925ff43104c408e6afb6cfb0e7df2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 07:18:17 -0700

    add more numbers to the form, add rendering

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 //grab elements from the DOM
-// const number = document.getElementById("numberBox");
-// const goButton = document.getElementById("go-btn");
-// const result = document.getElementById("resultDiv");
-// const renderResult = document.createElement("p");
+const number1 = document.getElementById("numberBox1");
+const number2 = document.getElementById("numberBox2");
+const number3 = document.getElementById("numberBox3");
+const number4 = document.getElementById("numberBox4");
+const number5 = document.getElementById("numberBox5");
+
+const goButton = document.getElementById("go-btn");
+const result = document.getElementById("resultDiv");
+const renderResult = document.createElement("p");
 
 //get the square root of each the number the user enters, then sum up the total
 
 function getSquareRoot(numbers) {
     for (let i = 0; i<numbers.length; i++) {
-        //renderResult.innerHTML = "";
-        //renderResult.innerHTML = Math.sqrt([i]);
-        //result.append(renderResult);
-        console.log(Math.sqrt([i]));
+        renderResult.innerHTML = "";
+        renderResult.innerHTML = Math.sqrt([i]);
+        result.append(renderResult);
+        //console.log(Math.sqrt([i]));
     }
    
 }
 
-console.log(getSquareRoot([9, 5, 7, 4]));
+//console.log(getSquareRoot([9, 5, 7, 4]));
 
-// goButton.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     return getSquareRoot(number.value);
-// })
+goButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("it worked!!");
+    return getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
+})
