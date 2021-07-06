53d3e9ebae3b2d63c9d0a5e725a0aa8ff5de52b6
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-07-06 08:46:35 -0700

    try adding a clear button

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const number4 = document.getElementById("numberBox4");
 const number5 = document.getElementById("numberBox5");
 
 const goButton = document.getElementById("go-btn");
+const clearButton = document.getElementById("clearBtn");
 const result = document.getElementById("resultDiv");
 let renderResult = document.createElement("p");
 
 //get the square root of each the number the user enters, then sum up the total
 
 function getSquareRoot(numbers) {
-    
+    goButton.classList.toggle("hidden");
     for (let i = 0; i<numbers.length; i++) {
-        //result.remove("p");
         let renderResult = document.createElement("p");
         renderResult.innerHTML = "";
         renderResult.innerHTML = `The Square Root of ${numbers[i]} is ${Math.sqrt(numbers[i])}`;
@@ -24,11 +24,15 @@ function getSquareRoot(numbers) {
     }
 }
 
-console.log(getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]));
+function clearArray(numbers) {
+    numbers.splice(0);
+    clearButton.classList.toggle("hidden");
+}
+
+//console.log(getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]));
 
 goButton.addEventListener('click', function(e) {
     e.preventDefault();
     //console.log("it worked!!");
-    renderResult.innerHTML = "";
     return getSquareRoot([parseInt(number1.value), parseInt(number2.value), parseInt(number3.value), parseInt(number4.value), parseInt(number5.value)]);
 })
